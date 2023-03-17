49692254c05cda4b96a8a129ecaad724ded4ddab
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-03-17 10:48:06 -0500

    Set variable of Char_Info equal to array including [Char_Name, Char_Race, Char_ Class, CurrentHitPoints, MaxHitPoints, Armor_class]. Added variable of Relentless_Endurance_Active, initially set to true

--- Scripts/charInfo.js
+++ Scripts/charInfo.js
@@ -4,3 +4,5 @@ let Char_Class = String;
 let CurrentHitPoints = Number;
 const MaxHitPoints = Number;
 let Armor_class = Number;
+const Char_Info = [Char_Name, Char_Race, Char_Class, CurrentHitPoints, MaxHitPoints, Armor_class];
+let Relentless_Endurance_Active = true
