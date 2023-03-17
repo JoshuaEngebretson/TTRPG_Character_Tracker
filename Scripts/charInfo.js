bf72f9e1a7fb083e5da10791cf2c742d74222b81
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-03-17 11:48:55 -0500

    Adjusted variables in Char_Info array to better check and log the responses

--- Scripts/charInfo.js
+++ Scripts/charInfo.js
@@ -5,7 +5,7 @@ let CurrentHitPoints = Number;
 let MaxHitPoints = Number;
 let Armor_class = Number;
 
-Char_Name = 'Wambamnec';
+Char_Name = 'Wam';
 Char_Race = 'Half-Orc';
 Char_Class = 'Fighter';
 CurrentHitPoints = 10;
@@ -13,7 +13,7 @@ MaxHitPoints = 15;
 Armor_class = 15;
 
 
-let Char_Info = ['Character Name = '+Char_Name, 'Character Race = '+Char_Race, 'Class = '+Char_Class, 'Current Hit Points:', CurrentHitPoints, 'Max Hit Points:', MaxHitPoints, 'Armor Class:', Armor_class];
+let Char_Info = ['Character Name:', Char_Name, 'Character Race:', Char_Race, 'Class = '+Char_Class, 'Current Hit Points:', CurrentHitPoints, 'Max Hit Points:', MaxHitPoints, 'Armor Class:', Armor_class];
 let Relentless_Endurance_Active = true
 
 console.log(Char_Info);
@@ -21,31 +21,31 @@ console.log(Char_Info);
 
 //Function to add or remove hitpoints to a character's total hp
 function add_or_remove_HitPoints( Pos_or_Neg_HP_adjustment){
-    Char_Info[4] += Pos_or_Neg_HP_adjustment
+    Char_Info[6] += Pos_or_Neg_HP_adjustment
     // If Positive number, add to HP
     if( Pos_or_Neg_HP_adjustment > 0 ){
         // Check if at or below MaxHitPoints
-        if (Char_Info[4] >= Char_Info[6]) {
-            Char_Info[4] = Char_Info[6]
-            return `${Char_Info[0]} has healed ${Pos_or_Neg_HP_adjustment} to a max of ${Char_Info[4]} hp.`
+        if (Char_Info[6] >= Char_Info[8]) {
+            Char_Info[6] = Char_Info[8]
+            return `${Char_Info[1]} has healed ${Pos_or_Neg_HP_adjustment} to a max of ${Char_Info[8]} hp.`
         }
-        return `${Char_Info[0]} has healed ${Pos_or_Neg_HP_adjustment} and is currently at ${Char_Info[4]} hp.`
+        return `${Char_Info[1]} has healed ${Pos_or_Neg_HP_adjustment} and is currently at ${Char_Info[5]} hp.`
     }
     // If Negative number, remove from HP
     else if ( Pos_or_Neg_HP_adjustment < 0 ){
-        if ( Char_Info[4] <= 0 && Char_Info[4] > -Char_Info[6]){
-            if ( Char_Info[1] === 'Half-Orc' && Relentless_Endurance_Active === True || Char_Info[1] === 'Orc' && Relentless_Endurance_Active === true ) {
+        if ( Char_Info[6] <= 0 && Char_Info[6] > -Char_Info[8]){
+            if ( Char_Info[3] === 'Half-Orc' && Relentless_Endurance_Active === true || Char_Info[3] === 'Orc' && Relentless_Endurance_Active === true ) {
                 Relentless_Endurance_Active = false
-                Char_Info[4] = 1
-                return `${Char_Info[0]} is at ${Char_Info[4]} hp. ${Char_Info[0]} has used Relentless_Endurance, you can't use this feature again until you finish a Long Rest.`
+                Char_Info[6] = 1
+                return `${Char_Info[1]} is at ${Char_Info[6]} hp. ${Char_Info[1]} has used Relentless Endurance and can't use this feature again until they finish a Long Rest.`
             }
-            if ( Char_Info[4] < 0 && Char_Info[4] <= -Char_Info[6]) {
-                return `${Char_Info[0]} has died.`
+            if ( Char_Info[6] < 0 && Char_Info[4] <= -Char_Info[8]) {
+                return `${Char_Info[1]} has died.`
             }
-            return `${Char_Info[0]} is unconcious, with a total hp of ${Char_Info[4]}.`
+            return `${Char_Info[1]} is unconcious, with a total hp of ${Char_Info[6]}.`
         }
     }
-    return `${Char_Info[0]} has ${Char_Info[4]} hp.`
+    return `${Char_Info[1]} has ${Char_Info[6]} hp.`
 }    
 
 //Testing function
@@ -61,5 +61,10 @@ console.log(Char_Info);
 console.log('add_or_remove_HitPoints - Should state -14:', add_or_remove_HitPoints(-29));
 console.log(Char_Info);
 
-console.log('add_or_remove_HitPoints - Should state "... has died.":', add_or_remove_HitPoints(-29));
+console.log('add_or_remove_HitPoints - Should state -7:', add_or_remove_HitPoints(7));
 console.log(Char_Info);
+
+console.log('add_or_remove_HitPoints - Should state "'+Char_Info[1], 'has died.":', add_or_remove_HitPoints(-29));
+console.log(Char_Info);
+
+console.log('Checking Relentless_Endurance_Active, should state false', Relentless_Endurance_Active);
