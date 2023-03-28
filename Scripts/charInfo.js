eb204fe9becfd0951be72f6c0d832b277ef0f5bb
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-03-27 23:35:04 -0500

    Added check to add_or_remove_HitPoints to test if already dead, if dead, unable to heal (will need to add if dead unable to take further damage). Further built out longRest to only reset if character is not dead and is above 0 hp. Built out better readability surrounding when a function is being called in the log.

--- Scripts/charInfo.js
+++ Scripts/charInfo.js
@@ -22,17 +22,22 @@ let Seahawk_Info = {
 
 let arrayOfCharactersInfo = [ Wam_Info, Seahawk_Info ];
 
+
 console.log( Wam_Info );
 console.log( Seahawk_Info );
 console.log( arrayOfCharactersInfo );
 
 
+
 //Function to add or remove hitpoints to a character's total hp
 function add_or_remove_HitPoints( Pos_or_Neg_HP_adjustment, Character ){
+    console.log(`-----\nusing add_or_remove_HitPoints function to adjust hp for ${Character.Name}\n-----`);
     let MaxHitPoints = Character.MaxHitPoints;
     let CurrentHitPoints = Character.CurrentHitPoints;
     let Negative_MaxHitPoints = (MaxHitPoints * -1);
-    console.log(Character.CurrentHitPoints);
+    if (Character.CurrentHitPoints === 'dead') {
+        return `${Character.Name} is dead and needs to be resurrected before they can be healed.`
+    }
     CurrentHitPoints += Pos_or_Neg_HP_adjustment;
     // If Positive number, add to HP
     if( Pos_or_Neg_HP_adjustment > 0 ){
@@ -71,7 +76,7 @@ function add_or_remove_HitPoints( Pos_or_Neg_HP_adjustment, Character ){
         else {
             console.log(`Test 6 (HP reduced, but not below 0)`);
             Character.CurrentHitPoints = CurrentHitPoints
-            return `${Character.Name} has taken ${`-`+Pos_or_Neg_HP_adjustment} points of damage and is now at ${Character.CurrentHitPoints}`
+            return `${Character.Name} has taken ${-(Pos_or_Neg_HP_adjustment)} point(s) of damage and is now at ${Character.CurrentHitPoints}`
         }
     }
 };
@@ -93,15 +98,38 @@ console.log( `add_or_remove_HitPoints - Should state ${Wam_Info.Name} has died. 
 console.log( `Checking Relentless_Endurance_Active for Wam, should state false ---> ${Wam_Info.Relentless_Endurance_Active}` );
 
 
-// console.log( `Wam's current character info ---> ${listCharacterInfo(Wam_Info)}` );
-console.log(`Below is Wam's current Info`);
-console.log(Wam_Info);
 
-function longRest(Character) {
+// console.log( `Wam's current character info ---> ${listCharacterInfo(Wam_Info)}` );
+console.log( `Below is Wam's current Info` );
+console.log( Wam_Info );
+
+function longRest( Character ) {
+    console.log(`-----\nusing longRest function to Reset ${Character.Name}'s per long rest traits\n-----`);
+    if ( Character.CurrentHitPoints === 'dead' ) {
+        return `${Character.Name} is dead and unable to gain the benefits of a long rest`
+    }
     Character.Relentless_Endurance_Active = true
     Character.actionSurge = true
+    if ( Character.CurrentHitPoints > 0 ){
+        Character.CurrentHitPoints = Character.MaxHitPoints
+    }
     return `${Character.Name} has taken a longRest`
 }
 
-console.log(longRest(Wam_Info));
+console.log( longRest(Wam_Info) );
+console.log( `Checking Relentless_Endurance_Active for Wam, should state false ---> ${Wam_Info.Relentless_Endurance_Active}` );
+console.log( Wam_Info );
+
+
+
+function ressurection( Character ) {
+    console.log(`-----\nUsing ressurection on ${Character.Name}\n-----`);
+    if ( Character.CurrentHitPoints === 'dead' ) {
+        Character.CurrentHitPoints = 1
+        return `${Character.Name} has been resurrected. ${Character.Name} is now at ${Character.CurrentHitPoints} hitpoint.`
+    }
+}
+
+console.log( ressurection(Wam_Info) );
+console.log( longRest(Wam_Info) );
 console.log( `Checking Relentless_Endurance_Active for Wam, should state true ---> ${Wam_Info.Relentless_Endurance_Active}` );
