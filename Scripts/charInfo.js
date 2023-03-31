6ac6bc3777357f018f56be5f8651b92f57156325
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-03-30 23:13:29 -0500

    Additional documentation and characterInfo -Added additional comments throughout code. 	*in longRest - added logs to know how much longer resurrectionSickness exists. 	*in WamTest - added additional logs to test if resurrectionSickness logs were working. 	*Corrected comment to end Add_RemoveHitPoints function -Added more info to Wam_Info and Seahawk_info 	*ProficientcyBonus 	*AbilityScores 	*SavingThrows 	*Skills -Built function abilityModifierCalc which calculates a character's ability score modifier based on their current Ability Score. 	*Logic is: 		**If an even number, subtract 10, then divide by 2 		**Else round down to even number then subtract 10, and divide by 2

--- Scripts/charInfo.js
+++ Scripts/charInfo.js
@@ -1,14 +1,54 @@
 let Wam_Info = {
     Name: 'Wam',
     Race: 'Half-Orc',
-    Class: 'Fighter',
+    Class: 'Fighter', // Adjust to be an object with class + level as properties. This can allow for multi-classing
     CurrentHitPoints: 10,
     MaxHitPoints: 15,
-    Armor_class: 15,
-    Negative_MaxHitPoints: -15,
-    Relentless_Endurance_Active: true,
-    actionSurge: true
+    Armor_class: 15, // Could more to Additional Info
+    Negative_MaxHitPoints: -15, // does this need to be set here? Could just be set in adjustHP function
+    Relentless_Endurance_Active: true, // Could move to sub property of Race - 'Half-Orc' || 'Orc'
+    actionSurge: true, // Could move to sub property of class - 'Fighter'
+    ProficiencyBonus: 5,
+    AbilityScores: [ // Modifier set via function that takes (AbilityScore(rounded down to even number) - 10) / 2 
+        {Ability: 'Strength', AbilityScore: 20},
+        {Ability: 'Dexterity', AbilityScore: 18},
+        {Ability: 'Constitution', AbilityScore: 21},
+        {Ability: 'Intelligence', AbilityScore: 17},
+        {Ability: 'Wisdom', AbilityScore: 10},
+        {Ability: 'Charisma', AbilityScore: 10}
+    ],
+    SavingThrows: [ // SavingThrows Bonus needs to be set via function that tests AbilityScores.Modifier + if(Proficient === true){proficiencyBonus} + MiscBonus
+        {Ability: 'Strength', Proficient: true},
+        {Ability: 'Dexterity', Proficient: false},
+        {Ability: 'Constitution', Proficient: true},
+        {Ability: 'Intelligence', Proficient: false},
+        {Ability: 'Wisdom', Proficient: false},
+        {Ability: 'Charisma', Proficient: false}
+    ],
+    Skills: [ // SkillBonus needs to be set via function that tests AbilityScores.Modifier + if(Proficient === true){proficiencyBonus} + MiscBonus
+        {SkillName: 'Acrobatics', Ability: '(Dex)', Proficient: false},
+        {SkillName: 'Animal Handline', Ability: '(Wis)', Proficient: false},
+        {SkillName: 'Arcana', Ability: '(Int)', Proficient: false},
+        {SkillName: 'Athletics', Ability: '(Str)', Proficient: true},
+        {SkillName: 'Deception', Ability: '(Cha)', Proficient: false},
+        {SkillName: 'History', Ability: '(Int)', Proficient: false},
+        {SkillName: 'Insight', Ability: '(Wis)', Proficient: false},
+        {SkillName: 'Intimidation', Ability: '(Cha)', Proficient: true},
+        {SkillName: 'Investigation', Ability: '(Int)', Proficient: false},
+        {SkillName: 'Medicine', Ability: '(Wis)', Proficient: false},
+        {SkillName: 'Nature', Ability: '(Int)', Proficient: false},
+        {SkillName: 'Perception', Ability: '(Wis)', Proficient: true},
+        {SkillName: 'Performance', Ability: '(Cha)', Proficient: false},
+        {SkillName: 'Persuasion', Ability: '(Cha)', Proficient: false},
+        {SkillName: 'Religion', Ability: '(Int)', Proficient: false},
+        {SkillName: 'Sleight of Hand', Ability: '(Dex)', Proficient: false},
+        {SkillName: 'Stealth', Ability: '(Dex)', Proficient: false},
+        {SkillName: 'Survival', Ability: '(Wis)', Proficient: false},
+    ]
 };
+// let Wam_AdditionalInfo = {
+//     abilityModifierCalc(Wam_Info);
+// };
 
 let Seahawk_Info = {
     Name: 'Seahawk',
@@ -17,7 +57,44 @@ let Seahawk_Info = {
     CurrentHitPoints: 11,
     MaxHitPoints: 12,
     Armor_class: 14,
-    Negative_MaxHitPoints: -12
+    Negative_MaxHitPoints: -12,
+    ProficiencyBonus: 2,
+    AbilityScores: [ // Modifier set via function that takes (AbilityScore(rounded down to even number) - 10) / 2 
+        {Ability: 'Strength', AbilityScore: 15},
+        {Ability: 'Dexterity', AbilityScore: 13},
+        {Ability: 'Constitution', AbilityScore: 14},
+        {Ability: 'Intelligence', AbilityScore: 11},
+        {Ability: 'Wisdom', AbilityScore: 9},
+        {Ability: 'Charisma', AbilityScore: 16}
+    ],
+    SavingThrows: [ // SavingThrows Bonus needs to be set via function that tests AbilityScores.Modifier + if(Proficient === true){proficiencyBonus} + MiscBonus
+        {Ability: 'Strength', Proficient: false},
+        {Ability: 'Dexterity', Proficient: true},
+        {Ability: 'Constitution', Proficient: false},
+        {Ability: 'Intelligence', Proficient: false},
+        {Ability: 'Wisdom', Proficient: false},
+        {Ability: 'Charisma', Proficient: true}
+    ],
+    Skills: [  // SkillBonus needs to be set via function that tests AbilityScores.Modifier + if(Proficient === true){proficiencyBonus} + MiscBonus
+        {SkillName: 'Acrobatics', Ability: '(Dex)', Proficient: false},
+        {SkillName: 'Animal Handline', Ability: '(Wis)', Proficient: true},
+        {SkillName: 'Arcana', Ability: '(Int)', Proficient: false},
+        {SkillName: 'Athletics', Ability: '(Str)', Proficient: true},
+        {SkillName: 'Deception', Ability: '(Cha)', Proficient: true},
+        {SkillName: 'History', Ability: '(Int)', Proficient: false},
+        {SkillName: 'Insight', Ability: '(Wis)', Proficient: false},
+        {SkillName: 'Intimidation', Ability: '(Cha)', Proficient: false},
+        {SkillName: 'Investigation', Ability: '(Int)', Proficient: false},
+        {SkillName: 'Medicine', Ability: '(Wis)', Proficient: false},
+        {SkillName: 'Nature', Ability: '(Int)', Proficient: false},
+        {SkillName: 'Perception', Ability: '(Wis)', Proficient: true},
+        {SkillName: 'Performance', Ability: '(Cha)', Proficient: false},
+        {SkillName: 'Persuasion', Ability: '(Cha)', Proficient: true},
+        {SkillName: 'Religion', Ability: '(Int)', Proficient: false},
+        {SkillName: 'Sleight of Hand', Ability: '(Dex)', Proficient: false},
+        {SkillName: 'Stealth', Ability: '(Dex)', Proficient: false},
+        {SkillName: 'Survival', Ability: '(Wis)', Proficient: false},
+    ]
 };
 
 let arrayOfCharactersInfo = [ Wam_Info, Seahawk_Info ];
@@ -79,7 +156,7 @@ function Add_RemoveHitPoints( Pos_or_Neg_HP_adjustment, Character ){
             return `💔${Character.Name} has taken ${-(Pos_or_Neg_HP_adjustment)} point(s) of damage and is now at ${Character.CurrentHitPoints}💔`
         }
     }
-}; // end add_or_remove_HitPoints
+}; // end Add_RemoveHitPoints
 
 
 function longRest( Character ) {
@@ -116,11 +193,13 @@ function longRest( Character ) {
         */
     } // End test based on .Class
     if( Character.ResurrectionSickness ){ // if ResurrectionSickness
-        if ( Character.ResurrectionSickness >= 0 ) { // if RessurrectionSickness is greater than or equal to 0
+        if ( Character.ResurrectionSickness >= -1 ) { // if RessurrectionSickness is greater than or equal to 0
             delete Character.ResurrectionSickness // removes property of ResurrectionSickness
+            console.log(`${Character.Name} is no longer suffering from Resurrection Sickness`);
         }
         else { // ELSE (assumes RessurrectionSickness is less than 0)
             Character.ResurrectionSickness += 1 // reduce negative of ResurrectionSickness
+            console.log(`${Character.Name} needs ${-Character.ResurrectionSickness} more long rests until Resurrection Sickness wears off.`);
         }
     } // End if ResurrectionSickness
     return `${Character.Name} has taken a longRest and is now at max hitpoints - ${Character.CurrentHitPoints} 💚`
@@ -157,7 +236,14 @@ function WamTest() { // Console Logs of Wam Tests
     console.log( `Checking Relentless_Endurance_Active for Wam, Expected false ---> ${Wam_Info.Relentless_Endurance_Active}` );
     console.log( resurrection(Wam_Info) );
     console.log( longRest(Wam_Info) );
+    console.log( '👆1st long rest since death, expect 3 more rests needed👆' );
     console.log( `Checking Relentless_Endurance_Active for Wam, Expected true ---> ${Wam_Info.Relentless_Endurance_Active}` );
+    console.log( longRest(Wam_Info) );
+    console.log( '👆2nd long rest since death, expect 2 more rests needed👆' );
+    console.log( longRest(Wam_Info) );
+    console.log( '👆3rd long rest since death, expect 1 more rests needed👆' );
+    console.log( longRest(Wam_Info) );
+    console.log( '👆4th long rest since death, expect 0 more rests needed👆' );
     console.log( `👇 👇 Wam's current info AFTER Tests 👇 👇` );
     console.log( Wam_Info );
     console.log(`Finished WamTest\n########################################`);
@@ -191,3 +277,17 @@ function SeahawkTest() { // Console Logs of Seahawk Tests
     console.log(`Finished SeahawkTest\n########################################`);
 } // End Seahawk Tests
 SeahawkTest()
+
+
+function abilityModifierCalc (Character_Info) { // Calculates ability score modifier
+    console.log(Character_Info.AbilityScores);
+    for (let i=0; i<Character_Info.AbilityScores.length; i++) {
+        if (Character_Info.AbilityScores[i].AbilityScore%2 === 0) {
+            Character_Info.AbilityScores[i].Modifier = ((Character_Info.AbilityScores[i].AbilityScore-10)/2)
+        }
+        else {
+            let ModRound = Character_Info.AbilityScores[i].AbilityScore -1
+            Character_Info.AbilityScores[i].Modifier = ((ModRound-10)/2)
+        }
+    };
+} // End abilityModifierCalc
